72f826c653e71e8b2d49d96dbe8097b097f34fe1
Vincent Barrault <vbarrault@oxent.net>
2016-06-17 19:03:27 +0200

    feat(index): match row template

--- main.js
+++ main.js
@@ -34,51 +34,28 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();
         toornamentApi.callApi('match_list', {'tournamentId': e.target.dataset.id},
             function (data) {
+                matchList.innerHTML = '';
+                data.forEach(function (value) {
+                    interpretedDate = moment(value.date, 'YYYY-MM-DDTHH:mm:ssZZ');
+                    matchList.innerHTML += Mustache.render(matchRowTemplate, {
+                        'match_id':       value.id,
+                        'tournament_id':  value.tournament_id,
+                        'stage_number':   value.stage_number,
+                        'group_number':   value.group_number,
+                        'round_number':   value.round_number,
+                        'opponent1_name': value.opponents[0].participant !== null? value.opponents[0].participant.name: '',
+                        'opponent2_name': value.opponents[1].participant !== null? value.opponents[1].participant.name: '',
+                        'date':           interpretedDate.isValid()? interpretedDate.format('L'): '',
+                        'time':           interpretedDate.isValid()? interpretedDate.format('LT'): ''
+                    });
+                });
+
+                [].forEach.call(matchList.getElementsByClassName('button-save-match'), function(element) {
+                    element.addEventListener('click', saveMatchAction);
+                });
+
                 for(i=0; i<views.length; i++) {
                     views[i].className = 'view hide';
-                }
-                matchList.innerHTML = '';
-
-                for(i=0; i<data.length; i++) {
-                    match_id = data[i].id;
-                    tournament_id = data[i].tournament_id;
-                    stage_number = data[i].stage_number;
-                    group_number = data[i].group_number;
-                    round_number = data[i].round_number;
-                    opponent1_name = data[i].opponents[0].participant !== null? data[i].opponents[0].participant.name: '';
-                    opponent2_name = data[i].opponents[1].participant !== null? data[i].opponents[1].participant.name: '';
-
-                    date = '';
-                    time = '';
-                    interpretedDate = moment(data[i].date, 'YYYY-MM-DDTHH:mm:ssZZ');
-                    if (interpretedDate.isValid()){
-                        date = interpretedDate.format('L');
-                        time = interpretedDate.format('LT');
-                    }
-
-                    row =   '<tr>' +
-                        '<td class="data column-large">' +
-                        '<div class="grid-flex vertical">' +
-                        '<div><span style="font-size: 12px;">Stage ' + stage_number + ' / Group ' + group_number + ' / Round ' + round_number + '</span></div>' +
-                        '<div><strong>' + opponent1_name + '</strong>' + ' vs ' + '<strong>' + opponent2_name + '</strong></div>' +
-                        '</div>' +
-                        '</td>' +
-                        '<td class="data column-medium"><div class="form-row"><div class="form-field"><input type="text" class="date" value="' + date + '" placeholder="JJ/MM/AAAA" /></div></div></td>' +
-                        '<td class="data column-medium"><div class="form-row"><div class="form-field"><input type="text" class="time" value="' + time + '" placeholder="HH:MM" /></div></div></td>' +
-                        '<td class="actions column-small">' +
-                        '<ul class="button-list layout-right collapsed">' +
-                        '<li><a data-match="' + match_id + '" data-tournament="' + tournament_id + '" class="button button-save-match light"><i class="fa-pencil"></i></a></li>' +
-                        '</ul>' +
-                        '</td>' +
-                        '</tr>'
-                    ;
-                    matchList.innerHTML += row;
-                }
-
-
-                var saveMatchButtons = matchList.getElementsByClassName('button-save-match');
-                for (var i=0; i<saveMatchButtons.length; i++){
-                    saveMatchButtons[i].addEventListener('click', saveMatchAction);
                 }
                 matchListContainer.className = 'view';
             },
@@ -118,11 +95,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 data.forEach(function (value) {
                     tournamentList.innerHTML += Mustache.render(tournamentRowTemplate, {
-                        'id':               value.id,
-                        'name':             value.name,
-                        'discipline':       value.discipline,
-                        'size':             value.size,
-                        'isPublic':         value.public?'public':'draft'
+                        'id':         value.id,
+                        'name':       value.name,
+                        'discipline': value.discipline,
+                        'size':       value.size,
+                        'isPublic':   value.public?'public':'draft'
                     });
                 });
 
